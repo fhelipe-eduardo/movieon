bad329ba065c8532d55e807b4e2f1407eaaca03a
Felipe Eduardo <felipegcbarros@gmail.com>
2026-06-03 19:21:39 -0300

    :sparkles: add menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -88,3 +88,20 @@ document
                 submitButton.disabled = false;
             });
     });
+
+// Menu Hamburguer
+const hamburger = document.querySelector(".nav-hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
+});
+
+// Fecha o menu ao clicar em um link
+document.querySelectorAll(".nav-links a").forEach((link) => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+        hamburger.classList.remove("active");
+    });
+});
